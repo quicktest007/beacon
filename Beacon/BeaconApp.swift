6523e3b9672dedf800db1e796386987834d5598d
Brewy <jd.hcovenanteyes@gmail.com>
2026-01-15 09:52:10 -0500

    Initial Commit

---
+++ Beacon/BeaconApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeaconApp.swift
+//  Beacon
+//
+//  Created by JD Hadley on 1/15/26.
+//
+
+import SwiftUI
+
+@main
+struct BeaconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
